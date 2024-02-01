d72d682c4d2db4b6e9844459de5fa1cea7f2a6fe
sheng_bu <sheng_bu@intsig.net>
2024-02-01 15:17:18 +0800

    .

--- app/main.go
+++ app/main.go
@@ -3,7 +3,7 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/inoth/promcollectr/cmd"
+import "github.com/inoth/promcollectr/app/cmd"
 
 func main() {
 	cmd.Execute()
